81e6cd4db7df4c03e5b9872902e77700f7ddb55d
Wooodstock <bertrand.heuillard@gmail.com>
2014-03-12 18:10:53 +0100

    Add Crowling ObjC Class

---
+++ NaziBabySlaughter.spritebuilder/Source/Crawling.m
@@ -0,0 +1,23 @@
+//
+//  Crawling.m
+//  NaziBabySlaughter
+//
+//  Created by Bertrand on 3/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Crawling.h"
+
+@implementation Crawling
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Crawling created");
+    }
+    
+    return self;
+}
+
+@end
