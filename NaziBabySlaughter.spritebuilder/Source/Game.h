0ce0b8fe1c2942d57a25171c923adbfe6b4f1043
thibautpalier <thibault.palier@viacesi.fr>
2014-03-17 20:12:40 +0100

    Add Game class

---
+++ NaziBabySlaughter.spritebuilder/Source/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  NaziBabySlaughter
+//
+//  Created by Thibault Palier on 17/03/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Game : CCNode
+
+@end
